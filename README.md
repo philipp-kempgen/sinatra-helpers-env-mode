87f05ff6c9e7f97e125f7ac11dba9a7c39f925df
Philipp Kempgen <lists@kempgen.net>
2013-04-16 15:53:22 +0200

    cleanup

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Helpers for environment mode handling in Sinatra (development, test, production)
 
 Author: Philipp Kempgen, [http://kempgen.net](http://kempgen.net)
 
+
+## Usage
+
+Include the module in your Sinatra application:
+
+	helpers ::Sinatra::Helpers::Env
+
--- lib/sinatra-helpers/env.rb
+++ lib/sinatra-helpers/env.rb
@@ -1,9 +1,24 @@
 require 'sinatra/base'
 
+# see Sinatra
 module Sinatra
+	# see Sinatra
 	module Helpers
+		
+		# Environment mode helpers.
+		#
+		# Usage:
+		#
+		# Include the module in your Sinatra application:
+		#
+		#   helpers ::Sinatra::Helpers::Env
+		#
 		module Env
 			
+			# Returns the environment mode.
+			#
+			# @return           [Symbol]   The mode.
+			#
 			def env_mode
 				[ settings.environment, ::ENV['RACK_ENV'],
 				].each { |mode|
@@ -12,8 +27,13 @@ module Sinatra
 				return :development
 			end
 			
+			# Checks for a given environment mode.
+			#
+			# @param   mode     [Symbol]   The mode to check for.
+			# @return                      `true` or `false`
+			#
 			def env_mode?( mode )
-				return mode.to_sym == self.env_mode
+				return self.env_mode == mode.to_sym
 			end
 			
 		end
